aec830319c6b54ac00fe0953a3da6c10bc2d01f9
Chris <christopherortega56@gmail.com>
2018-09-14 19:47:15 -0500

    adding background and navigation with tachyons styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './Component/Navigation/Navigation.js'
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Navigation/Logo'
 
 class App extends Component {
-
-
-
   render() {
     return (
       <div className="App">
       <Navigation/>
+      <Logo/>
         {
-        //<Logo/>
         //<ImageLinkForm/>
         //<FaceRecognition/>
         }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,8 +3,8 @@ import React, {Component} from  'react';
 class Navigation extends Component {
 	render(){
 		return (
-			<nav>
-				<p> sign out </p>
+			<nav style = {{display:'flex', justifyContent:'flex-end'}}>
+				<p className = 'f3 link dim black underline pa3 pointer'> sign out </p>
 			</nav>
 
 		)

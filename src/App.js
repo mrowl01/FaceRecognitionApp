a71df40f696b8f58933d763bf869ef0e0a61a2b5
Chris <christopherortega56@gmail.com>
2018-09-14 19:19:13 -0500

    starting project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './Component/Navigation/Navigation.js'
+
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <div className="App">
+      <Navigation/>
+        {
+        //<Logo/>
+        //<ImageLinkForm/>
+        //<FaceRecognition/>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React, {Component} from  'react';
+
+class Navigation extends Component {
+	render(){
+		return (
+			<nav>
+				<p> sign out </p>
+			</nav>
+
+		)
+	}
+}
+
+export default Navigation ; 

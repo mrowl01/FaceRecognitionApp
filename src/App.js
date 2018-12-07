81b18df380035763ddeb35003a5dad5a77793287
Chris <chrisortegamusic@gmail.com>
2018-12-07 07:02:58 -0600

    fixing typo causing error with backend

--- src/App.js
+++ src/App.js
@@ -106,7 +106,7 @@ class App extends Component {
   }
   onSubmit = () =>{
     this.setState({imageURL:this.state.input});
-    fetch('https://young-everglades-13596.herokuapp.com//imageurl', {
+    fetch('https://young-everglades-13596.herokuapp.com/imageurl', {
           method:'post', 
           headers: {'Content-Type':'application/json'},
           body:JSON.stringify({
@@ -116,7 +116,7 @@ class App extends Component {
     .then(response=>response.json())
     .then(response=> {
       if(response){
-        fetch('https://young-everglades-13596.herokuapp.com//image', {
+        fetch('https://young-everglades-13596.herokuapp.com/image', {
           method:'put', 
           headers: {'Content-Type':'application/json'},
           body:JSON.stringify({
